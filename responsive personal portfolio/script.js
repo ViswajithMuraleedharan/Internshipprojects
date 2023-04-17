e9104abaa19e169420012f6b72585a5f0a8dc754
Viswajith Muraleedharan <viswajithmuralee1@gmail.com>
2023-04-17 12:31:01 +0530

    add all bootstrap

---
+++ responsive personal portfolio/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){   //make the document ready for function
+    $('.burger-menu').on('click', function(){ //function selects class '.burger-menu' on click and calls afunction
+        $('.mob-nav').toggle();  //to toggle it. By toggling the css code of '.mobile-nav'and changing its function to the opposite of specified value.
+    })
+});
+//$ stands for jQuery.
